bcb8724b94fd887624ea680092c4dc5e3ecaa27e
Cynicalbird <alex.ng.personal@gmail.com>
2017-01-31 23:24:11 -0500

    Version before pull from github

--- demo/script.js
+++ demo/script.js
@@ -1,19 +1,18 @@
 function loadJSON()
 {
+	//HTTP request to server
 	var request = new XMLHttpRequest();
 	request.open('GET', 'player.json', true);
+	
 	request.onload = function() 
 	{
+		//Successfull request
 		if (request.status >= 200 && request.status < 400) 
 		{
-			// Success!
-			var data = JSON.parse(request.responseText);
 			parseJSON(request.responseText);
 		} 
-		else 
-		{
-			// We reached our target server, but it returned an error
-		}
+		//Unsuccessfull request
+		else {}
 	};
 
 	request.onerror = function() 
