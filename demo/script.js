65a34fcf8184cbd190db07e58df68517c5b34172
Cynicalbird <alex.ng.personal@gmail.com>
2017-02-01 09:01:47 -0500

    Added more robust parsing for JSON now loops thought all fields

--- demo/script.js
+++ demo/script.js
@@ -1,3 +1,6 @@
+var player_field = document.getElementById("player_field"); 
+var player;
+
 function loadJSON()
 {
 	//HTTP request to server
@@ -26,4 +29,36 @@ function loadJSON()
 function parseJSON(text)
 {
 	alert(text);
+	player = JSON.parse(text);
+	displayPlayer(player);
+	storeJSON(player);
+}
+
+function storeJSON(JSONobject)
+{
+	JSONtext = JSON.stringify(JSONobject);
+	localStorage.setItem("JSONtext", JSONtext);
+}
+
+function displayPlayer(player)
+{
+	var playerHTML;
+	for (var attribute in player)
+	{
+		alert(attribute);
+		if (attribute.stringify != "undefined")
+		{
+			playerHTML = playerHTML + "<p>"+attribute+":"+player[attribute]+"</p><br>";
+		} 
+	}
+	player_field.innerHTML = playerHTML;
+}
+
+/* Simple but limmited compare method */
+function compare(object1, object2)
+{
+	if (object1.stringify() == object2.stringify())
+		return true;
+	else 
+		return false;
 }
