ff0e37fdd6adb426e0e793183d5b18201d5250bb
Alex Ng <alex.ng.personal@gmail.com>
2017-01-31 23:15:38 -0500

    Added a JSON get request.

--- demo/script.js
+++ demo/script.js
@@ -1,11 +1,29 @@
-
-
-function initializeGame()
+function loadJSON()
 {
-	alert("Game started");
-	var playerdata = '{ name: "alex", level: "0" }';
-	alert(playerdata);
-	var player = JSON.parse(playerdata);
-	alert(player);
-	alert("Game ended");		
+	//HTTP request to server
+	var request = new XMLHttpRequest();
+	request.open('GET', 'player.json', true);
+	
+	request.onload = function() 
+	{
+		//Successfull request
+		if (request.status >= 200 && request.status < 400) 
+		{
+			parseJSON(request.responseText);
+		} 
+		//Unsuccessfull request
+		else {}
+	};
+
+	request.onerror = function() 
+	{
+	// There was a connection error of some sort
+	};
+
+	request.send();
+}
+
+function parseJSON(text)
+{
+	alert(text);
 }
