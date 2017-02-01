c53120e6cabf4e072be41f10f988b65d7eeda8c4
Cynicalbird <alex.ng.personal@gmail.com>
2017-01-31 22:56:06 -0500

    added a demo JSON get request

--- demo/script.js
+++ demo/script.js
@@ -1,11 +1,30 @@
-
-
-function initializeGame()
+function loadJSON()
 {
-	alert("Game started");
-	var playerdata = '{ name: "alex", level: "0" }';
-	alert(playerdata);
-	var player = JSON.parse(playerdata);
-	alert(player);
-	alert("Game ended");		
+	var request = new XMLHttpRequest();
+	request.open('GET', 'player.json', true);
+	request.onload = function() 
+	{
+		if (request.status >= 200 && request.status < 400) 
+		{
+			// Success!
+			var data = JSON.parse(request.responseText);
+			parseJSON(request.responseText);
+		} 
+		else 
+		{
+			// We reached our target server, but it returned an error
+		}
+	};
+
+	request.onerror = function() 
+	{
+	// There was a connection error of some sort
+	};
+
+	request.send();
+}
+
+function parseJSON(text)
+{
+	alert(text);
 }
