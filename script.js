b27432d51e303d01061e19b4384ebf38bba73708
Cynicalbird <alex.ng.personal@gmail.com>
2017-02-03 15:40:01 -0500

    added basic character creation

--- script.js
+++ script.js
@@ -1,16 +1,57 @@
-function loadPlayerData()
-{
-        var ourRequest = new XMLHttpRequest();
-        ourRequest.open('GET', 'part2.json');
-        ourRequest.onload = function(){
-        if(ourRequest.status>=200 && ourRequest.status<400)	
-	{
-        	var ourData = JSON.parse(ourRequest.responseText);
-                renderHTML(ourData);
-      	} 
-	else
-	{
-        	console.log("server error");
-        }
+var player = new Object();
+
+//Basic player prototype WIP
+/*
+var player = {
+	name,
+	level,
+	hp,
+	maxhp,
+	exp,
+	ramz,
+	hair,
+	face,
+	eyes,
+	skin,
+	flacility,
+	wins,
+	loss
+};
+*/
+
+function storePlayerData()
+{	
+	var name_field = document.getElementById("name_field");
+	//var facility = document.getElementById("facility_field");
+
+	player.name = name_field.value;
+	player.hp = 50;
+	alert("Player name stored: "+player.name);	
+	//player.facility = facility.value;					
 }
+
+function loadJSON()
+{
+	var request = new XMLHttpRequest();
+	request.open('GET', 'player.json', true);
+	request.onload = function()
+	{
+		if(ourRequest.status>=200 && ourRequest.status<400)
+		{
+			var ourData = JSON.parse(ourRequest.responseText);
+			parseJSON(request.responseText);
+		} 
+		else
+		{
+			console.log("server error");
+		}
+	}
+}
+
+/*
+function parseJSON()
+{
+
+}
+*/
 
