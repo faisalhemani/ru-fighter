63ab30980053b827f85d0860481ee340e844337b
Cynicalbird <alex.ng.personal@gmail.com>
2017-02-20 17:11:42 -0500

    added assets phaser js company logo

--- game_engine/js/main.js
+++ game_engine/js/main.js
@@ -4,14 +4,17 @@ var GameState =
 {
 	preload : function()
 	{
-
+		game.load.image('logo', 'assets/logo.png');
 	},
 	create: function()
 	{
-
+		this.logo = this.game.add.sprite(0,0, 'logo');
 	},
 	update: function()
 	{
 
 	}
 };
+
+game.state.add("GameState", GameState);
+game.state.start("GameState");
