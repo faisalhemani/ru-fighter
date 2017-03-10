9d83052bb80d9a4d17a21dc7e1c4e85bc2b75566
Ubuntu <ubuntu@ip-172-31-30-208.us-west-2.compute.internal>
2017-03-10 21:48:44 +0000

    title to sign up screen

--- game_engine/js/CharSelect/selection.js
+++ game_engine/js/CharSelect/selection.js
@@ -1,16 +1,85 @@
 var game;
 var speedMult= 0.7;
 var friction  =  0.99;
-
+var chars = ["Faisal", "Xavier"];
 window.onload = function () {
-	game =  new Phaser.Game (320,480, Phaser.AUTO, "");
+	game =  new Phaser.Game (2000,2000, Phaser.AUTO, "");
 	game.state.add("PlayGame", playGame);	
-	game.state.start(playGame);
+	game.state.start("PlayGame");
 }
 var playGame = function (game) {};
 playGame.prototype = {
 	preload: function () {
-		game.load.image(
+
+		game.load.image("Faisal", "http://52.38.67.158/game_engine/js/CharSelect/faisal.png");
+		game.load.image("Xavier", "http://52.38.67.158/game_engine/js/CharSelect/xavier.png");
+	},
+	create: function () {
+		game.stage.backgroundColor = "#5f503b";
+		game.add.text(game.width/2, 50, "Select your Faculty!", {font:"18px Helvetica", fill: "#ffffff"}).anchor.set(0.5);
+		this.scrollingMap = game.add.tileSprite(0, 0, game.width/2 + chars.length * 90 + 64, game.height, "transp");
+		this.scrollingMap.inputEnabled = true;
+		this.scrollingMap.input.enableDrag(false);
+		this.scrollingMap.savedPosition = new Phaser.Point(this.scrollingMap.x, this.scrollingMap.y);
+		this.scrollingMap.isBeingDragged = false;
+		this.scrollingMap.movingSpeed = 0;
+		this.scrollingMap.input.allowVerticalDrag = false;
+		this.scrollingMap.input.boundsRect = new Phaser.Rectangle(game.width - this.scrollingMap.width, game.height - this.scrollingMap.height, this.scrollingMap.width * 2 - game.width, this.scrollingMap.height * 2 - game.height);
+		for (var i = 0; i< chars.length; i++){
+			var character = game.add.image(game.width/2 + i *90, game.height/2, "Character");
+			character.anchor.set(0.5);
+			this.scrollingMap.addChild(character);
+		}
+		this.scrollingMap.events.onDragStart.add(function (){
+			this.scrollingMap.isBeingDragged = true;
+			this.scrollingMap.movingSpeed = 0;
+			}, this);
+		this.scrollingMap.events.onDragStop.add(function(){
+			this.scrollingMap.isBeingDragged = false;
+			}, this);
+	},
+	update: function(){
+		var zoomed = false;
+		for (var i = 0; i < this.scrollingMap.children.length; i++){
+			if (Math.abs(this.scrollingMap.children[i].world.x - game.width / 2) < 46 && !zoomed){
+				this.scrollingMap.getChildAt(i).scale.setTo(1.5);
+				zoomed = true; 
+			} else {
+				this.scrollingMap.getChildAt(i).scale.setTo(1);
+			} 
+		}
+		if (this.scrollingMap.isBeingDragged) {
+			this.scrollingMap.savedPosition = new Phaser.Point(this.scrollingMap.x, this.scrollingMap.y);
+		} else {
+			if (this.scrollingMap.movingSpeed > 1){
+				this.scrollingMap.x += this.scrollingMap.movingSpeed * Math.cos(this.scrollingMap.movingangle);
+				if (this.scrollingMap.x > game.width - this.scrollingMap.width){
+					this.scrollingMap.x = game.width - this.scrollingMap.width;
+					this.scrollingMap.movingSpeed *= 0.5;
+					this.scrollingMap.movingangle += Math.PI;
+				}
+				if (this.scrollingMap.x > 0){
+					this.scrollingMap.x = 0;
+					this.scrollingMap.movingSpeed *= 0.5;
+					this.scrollingMap.movingangle += Math.PI;
+				}
+				this.scrollingMap.movingSpeed *= friction;
+				this.scrollingMap.savedPosition = new Phaser.Point(this.scrollingMap.x, this.scrollingMap.y);
+			} else{
+				var point1 = new Phaser.Point(12,23);
+				var point2 = new Phaser.Point(34,67);
+                    		var distance = this.scrollingMap.savedPosition.distance(this.scrollingMap.position);
+				this.scrollingMap.savedPosition.angle = function(a, b){
+					return Math.atan2(a.y - b.y, a.x - b.x);
+				};
+                    		var angle = this.scrollingMap.savedPosition.angle(point1, point2);
+                   		if(distance > 4){
+                         		this.scrollingMap.movingSpeed = distance * speedMult;
+                         		this.scrollingMap.movingangle = angle;
+                    }
+			}
+
+		}
 	}
 
 }
--- game_engine/js/main.js
+++ game_engine/js/main.js
@@ -6,6 +6,41 @@ var start_button;
 var game_started = 0;
 var game;
 
+var button1;
+var button2;
+var button3;
+var button4;
+
+var player1;
+var player2;
+
+//user
+var hpText;
+var mpText;
+var spdText;
+var hp;
+var mp;
+var spd;
+
+//compuer
+var chpText;
+var cmpText;
+var cspdText;
+var chp;
+var cmp;
+var cspd;
+
+//screens 
+var signup_screen;
+var login_screen;
+var charselect_screen;
+var map_screen;
+var battle_screen;
+
+//signup button 
+var signupBtn;
+
+
 function getWidth() {
 	return window.innerWidth;
 }
@@ -33,6 +68,24 @@ function preload() {
 	game.load.image('background', 'assets/AmberLabs_logo.png');
 	game.load.image('opening_screen', 'assets/RUFighter_logo.png');
 	game.load.spritesheet('start_button', 'assets/button.png',150,90);
+
+	//buttons for the player 
+	game.load.spritesheet('button1', 'assets/button.png',200,100);
+	game.load.spritesheet('button2', 'assets/button.png',200,100);
+	game.load.spritesheet('button3', 'assets/button.png',200,100);
+	game.load.spritesheet('button4', 'assets/button.png',200,100);
+
+	//player
+	game.load.image('player1', 'assets/Faisal.png');
+	game.load.image('player2', 'assets/Xavier.png');
+
+	// screens
+	game.load.image('signup_screen', 'assets/signup_screen.jpg');
+        game.load.image('login_screen', 'assets/login_screen.png');
+	game.load.image('charselect_screen', 'assets/charactercreation_screen.jpg.png');
+        game.load.image('map_screen', 'assets/map_screen.png');
+
+	game.load.spritesheet('signupBtn' , 'assets/button.png',50,100);
 }
 
 function create() 
@@ -43,21 +96,53 @@ function create()
 	{
 		console.log("game starting");
 		clearScreen();
-		display_game();
+		signupScreen();
 	}
+
+}
+
+//sign up screen
+function signupScreen()
+{
+	console.log("in sign up screen");
+	signup_screen = game.add.sprite(0,0,'signup_screen');
+	signupBtn = game.add.button(300,600, 'signupbtn' , signup_action, this, 2,1,0);
+}
+
+function signup_action(){
+
+	console.log('clearing sign up screen and goig to char slection');
+	clearScreen();
+	charSelectScreen();
+}
+
+function charSelectScreen()
+{
+	cosole.log("in char select screen");
 
 }
 
 function display_game()
 {
-	graphics = game.add.graphics(0,0);
-	person2 = game.add.graphics(0,0);
-	graphics.beginFill(0xFF0000,1);
-	graphics.drawCircle(300,300,100);
-	person2.beginFill(0x00FF00, 1);
-	person2.drawCircle(600,300,100);	
+//	graphics = game.add.graphics(0,0);
+//	person2 = game.add.graphics(0,0);
+//	graphics.beginFill(0xFF0000,1);
+//	graphics.drawCircle(300,300,100);
+//	person2.beginFill(0x00FF00, 1);
+//	person2.drawCircle(600,300,100);
+
+	//display buttons 
+        button1 =  game.add.button(10,600, 'button1', button1_action, this, 2,1,0);
+	button2 =  game.add.button(220,600, 'button2', button2_action, this, 2,1,0);
+	button3 =  game.add.button(440,600, 'button3', button3_action, this, 2,1,0);
+	button4 =  game.add.button(660,600, 'button4', button4_action, this, 2,1,0);
+	
+	//calling server
+	callingServer();
+
 }
 
+//tile page
 function startScreen()
 {
 	game.stage.backgroundColor = '#000000';
@@ -75,7 +160,19 @@ function clearScreen()
 {
 	console.log("clearing screen");
 	//game.remove(opening_screen);
-	game.world.removeAll();
+	//game.
+	remove(opening_screen);
+	remove(start_button);
+	remove(background);
+	game.world.remove(background);
+	//game.world.removeAll();
+	//console.log("Here");
+}
+
+function remove(element)
+{
+	element.visible = false;
+	//element.remove();
 }
 
 function update() 
@@ -156,8 +253,86 @@ function actionOnClick()
 	clearScreen();
 	game_started = 1;
 	console.log(game_started);
-	display_game();
+	signupScreen();
 }
+
+/*
+function healthAction(){
+	text = game.add.text(50,50, "Your Health is : 10", {font: "65px Arial", fill: '#ff0000'});
+	text.anchore.set(0.5,0.5);
+}
+*/
+
+function callingServer(){
+	
+	var request = new XMLHttpRequest();
+	var url = 'http://52.38.67.158/player.json';
+	request.open('GET',url,true);
+	request.onload = function ()
+	{
+		if (request.status >= 200 && request.status < 400)
+		{
+			//do something 
+			storeJSON(JSON.parse(request.response));
+		}
+		else
+		{
+			console.log("URL could not be reached: "+url);
+			alert("game server could not be reached");
+		}
+	};
+	request.onerror = function ()
+	{
+		alert("error");
+	}
+	request.send();
+}
+
+function storeJSON(JSON_object)
+{
+	console.log(JSON_object.hp);
+	//user info 
+	hp = "HP: " + JSON_object.hp;
+	mp = "MP: " + JSON_object.maxhp;
+	spd = "SPD: " +  JSON_object.facility;
+	hpText = game.add.text(10,10, hp, {font: "25px Arial", fill: "#ff0000"});
+        mpText = game.add.text(100,10, mp, {font: "25px Arial", fill: "#ff0000"});
+        spdText = game.add.text(200,10,spd, {font: "25px Arial", fill: "#ff0000"});
+
+	//computer info 
+	chp = "HP: " + JSON_object.hp;
+        cmp = "MP: " + JSON_object.maxhp;
+        cspd = "SPD: " +  JSON_object.facility;
+        chpText = game.add.text(300,10, chp, {font: "25px Arial", fill: "#ff00ff"});
+        cmpText = game.add.text(400,10, cmp, {font: "25px Arial", fill: "#ff00ff"});
+        cspdText = game.add.text(500,10,cspd, {font: "25px Arial", fill: "#ff00ff"});
+
+
+	//players 
+	person1 = game.add.sprite(500,300,'player1');
+	person2 = game.add.sprite(400,320,'player2');
+
+}
+
+//button functions
+
+function button1_action(){
+	console.log('button 1');
+}
+
+function button2_action(){
+	console.log("button 2");
+}
+
+function button3_action(){
+	console.log("button 3");
+}
+
+function button4_action(){
+	console.log("button 4");
+}
+
+
 
 /*function resize() {
 	Width = getWidth();
