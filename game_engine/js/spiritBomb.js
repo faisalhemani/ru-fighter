b8d711b293480d5e69a0420360ebf496df55e6ef
alex ng <alexng@aang.local>
2017-03-20 15:46:04 -0400

    testing spiritbomb skill

--- game_engine/js/spiritBomb.js
+++ game_engine/js/spiritBomb.js
@@ -1,7 +1,7 @@
 function createSpiritBomb()
 {
 	var chargeLight = particles.explosions.create(0, game.height/2, 'redPartical');
-	chargeLight.scale.setTo(8,8);
+	chargeLight.scale.setTo(20,20);
 }
 
 function animateSpiritBomb()
