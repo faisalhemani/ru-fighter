1da1fa12ded8941283d0b8d37faee56c86904bea
Cynicalbird <alex.ng.personal@gmail.com>
2017-04-02 02:21:14 -0400

    displays player and enemy stats

--- public/javascripts/battle.js
+++ public/javascripts/battle.js
@@ -23,42 +23,108 @@ function topText()
 
 	console.log(enemy);
 
-	uhp_txt = game.add.text(110,30,"Hp : " + player.stats.hp, {
+	player_hp_display = game.add.text(110,30,"Hp : " + player.stats.hp, {
 		font: "20px Arial",
 		fill: "#ff0000",
 		align: "center" 
 	});
 
-    umana_txt = game.add.text(260,30,"Mp : " + player.stats.mana,{
+    player_mana_display = game.add.text(260,30,"Mp : " + player.stats.mana,{
                 font: "20px Arial",
                 fill: "#ff0000",
-                align: "center" });
+                align: "center" 
+    });
 
 	if(player.facility == "Science")
  		var uspeed = "3";
 	else
 		uspeed = "2";
 
-	uspeed_txt = game.add.text(410,30,"Speed : " + player.stats.speed,{
+	player_speed_display = game.add.text(410,30,"Speed : " + player.stats.speed,{
                 font: "20px Arial",
                 fill: "#ff0000",
-                align: "center" });
+                align: "center" 
+	});
 
-    chp_txt = game.add.text(560,30,"Hp : " + enemy.stats.hp,{
+    enemy_hp_display = game.add.text(560,30,"Hp : " + enemy.stats.hp,{
                 font: "20px Arial",
                 fill: "#00ff00",
-                align: "center" });
+                align: "center" 
+    });
 
 
-    cmana_txt = game.add.text(710,30,"Mp : " + enemy.stats.mana,{
+    enemy_mana_display = game.add.text(710,30,"Mp : " + enemy.stats.mana,{
                 font: "20px Arial",
                 fill: "#00ff00",
-                align: "center" });
+                align: "center" 
+	});
 
-	cspeed_txt = game.add.text(860,30,"Speed : " + enemy.stats.speed,{
+	enemy_speed_display = game.add.text(860,30,"Speed : " + enemy.stats.speed,{
                 font: "20px Arial",
                 fill: "#00ff00",
-                align: "center" });
+                align: "center" 
+	});
 
 	//playerSpeed(speed, cspeed);
+}
+
+
+function playerSpeed(user, computer)
+{
+	if(counter == 0)
+	{
+		console.log("checking which player will go first");
+		//chance("checking which player will go first");
+	 	random = game.rnd.integerInRange(0, 1);
+		//speed same
+		if(user == computer)
+		{
+			if(random == 1)
+			{
+				chance("You Will Go First, All The Best");
+				console.log("player will go first");
+				player_counter++;
+				 if(player.facility == "Science")
+			                science();
+	        		else
+	        		        engineering();
+			}
+			else
+			{
+				ai_counter++;
+	 			chance("AI Will Go First");
+				console.log("AI will go first");
+				if (computer == 3)
+	        	                aisci_attacks();
+		                else
+	                	        aieng_attacks();
+
+			}
+		}
+		else if (user > computer)
+		{
+			player_counter++;
+			 chance("You Will Go First, All The Best");
+			console.log("player will go first");
+			if(player.facility == "Science")
+	                        science();
+	                else
+		                engineering();
+		}
+		else
+		{
+			ai_counter++;
+		 	chance("AI Will Go First");
+			console.log("AI will go first");
+
+			if (computer == 3)
+				aisci_attacks();
+			else
+				aieng_attacks();
+		}
+
+		counter++;
+	}
+	else 
+		trueBattle();
 }
