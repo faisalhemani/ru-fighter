ed2f385a7653b814be1b87914fc7c8c9af1c966a
Cynicalbird <alex.ng.personal@gmail.com>
2017-04-02 04:11:02 -0400

    testing

--- public/javascripts/science.js
+++ public/javascripts/science.js
@@ -50,17 +50,19 @@ function damage(damage, animation, amount) {
 	if (turn === 'player')
 	{
 		animation(amount);
-		if (enemy.stats.hp - damage >= 0)
+		if (enemy.stats.hp - damage >= 0) {
 			enemy.stats.hp = enemy.stats.hp - damage;
+			console.log(enemy.stats.hp);
+			turn = ai;
+			//clearBattle();
+			topText();
+			AI();
+		}
 		else{
 			enemy.stats.hp = 0;
 			gameOver();
 		}
-		console.log(enemy.stats.hp);
-		turn = ai;
-		//clearBattle();
-		topText();
-		AI();
+		
 	}
 }
 
