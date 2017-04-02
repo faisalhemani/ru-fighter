cb4be96526fae0e071f38c57f9374b43087fd49a
Cynicalbird <alex.ng.personal@gmail.com>
2017-04-02 03:26:59 -0400

    testing

--- public/javascripts/kunai.js
+++ public/javascripts/kunai.js
@@ -1,10 +1,12 @@
 function createKunai()
 {
-	var kunai = particles.kunai.create(0, game.rnd.integerInRange(0,game.height-250), 'kunai');
-	console.log(particles.kunai.children.length);
-	kunai.animations.add('anim');
-	kunai.play('anim', 10, true);
-	kunai.scale.setTo(3,3);
+	if (particles.kunai.children.length <= 3)
+	{
+		var kunai = particles.kunai.create(0, game.rnd.integerInRange(0,game.height-250), 'kunai');
+		kunai.animations.add('anim');
+		kunai.play('anim', 10, true);
+		kunai.scale.setTo(3,3);
+	}
 }
 
 function animateKunai()
