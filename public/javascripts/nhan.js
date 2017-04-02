f92751f51829007f287bb56496a55b9908789ffb
Cynicalbird <alex.ng.personal@gmail.com>
2017-04-02 01:39:06 -0400

    testing

--- public/javascripts/nhan.js
+++ public/javascripts/nhan.js
@@ -6,6 +6,9 @@ function nhanclass()
 	nhan_bg = game.add.sprite(0,0,'nhan_bg');
         nhan_bg.scale.setTo(0.9,0.9);
 //	console.log("Background: " + nhan_bg);
+	console.log(enemy);
+	console.log(enemy.sprite);
+	console.log(enemy.sprite.scale);
 	enemy.sprite.scale.setTo(0.25,0.25);
 //	console.log("Nhan: " + nhan);
     //nhan.scale.setTo(0.25,0.25);
