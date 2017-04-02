9da8aa3e7a8423e379cc84cc1091a2b62e7555ea
Ubuntu <ubuntu@ip-172-31-30-208.us-west-2.compute.internal>
2017-04-02 04:55:52 +0000

    added node packages to package.json

--- app.js
+++ app.js
@@ -82,7 +82,7 @@ app.use('/news', news);
 //app.use('/players', players);
 
 //Set Port
-app.set('port', (process.env.PORT || 3000));
+app.set('port', (process.env.PORT || 3001));
 app.listen(app.get('port'), function()
 {
 		console.log('Server started on port '+ app.get('port'));
