a91b31fbb3b9e331f6269848212a36212a3ef229
Cynicalbird <alex.ng.personal@gmail.com>
2017-04-02 03:22:28 -0400

    testing

--- public/javascripts/battle.js
+++ public/javascripts/battle.js
@@ -119,6 +119,7 @@ function playerSpeed()
 }
 
 function AI(){
+	sleep(2000);
 	var choice = random.integerInRange(0,3);
 	switch (choice)
 	{
@@ -133,4 +134,8 @@ function AI(){
 	}
 	turn = 'player';
 	console.log('AI moved: '+choice);
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
